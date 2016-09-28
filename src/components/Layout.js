9794b777df6bca6c177aa1b84f8f243f84edb86c
Jovvi Johnson <jjohnsow@gmail.com>
2016-09-28 15:19:55 -0700

    first commit

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Layout extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <h1 className='text-center'>React Calculator</h1>
+      </div>
+    )
+  }
+}
