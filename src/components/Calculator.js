6e8464825eefd607f406f541dc99a12676725f85
Jovvi Johnson <jjohnsow@gmail.com>
2016-09-28 16:56:16 -0700

    skeleton

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 
+import Display from './Display'
+import Button from './Button'
+
 export default class Layout extends Component {
 
   render() {
     return (
       <div className="container">
         <div className="calculator">
-
+          <Display />
+          <div className="button-wrapper">
+            <Button value="1"/>
+            <Button value="2"/>
+            <Button value="3"/>
+            <Button value="+"/>
+            <br />
+            <Button value="4"/>
+            <Button value="5"/>
+            <Button value="6"/>
+            <Button value="-"/>
+            <br />
+            <Button value="7"/>
+            <Button value="8"/>
+            <Button value="9"/>
+            <Button value="x"/>
+            <br />
+            <Button value="0"/>
+            <Button value="."/>
+            <Button value="/"/>
+            <Button value="="/>
+          </div>
         </div>
       </div>
     )
---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+export default class Display extends Component {
+  render () {
+    return (
+      <div className="container">
+        <div>
+          <input type="text" id="display" />
+        </div>
+      </div>
+    )
+  }
+}
