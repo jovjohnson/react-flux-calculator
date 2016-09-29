8ab4234de411d86c8eccea74598b216b8ec20804
Jovvi Johnson <jjohnsow@gmail.com>
2016-09-29 14:40:50 -0700

    val log

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Button extends Component {
-  _clicked() {
-    console.log('clickT')
+  _clicked(e) {
+    console.log(e.target.value);
   }
   render () {
     return (
-      <button onClick={this._clicked} className="btn btn-info button">{this.props.value}</button>
+      <button onClick={this._clicked}
+      className="btn btn-info button" value={this.props.value}>{this.props.value}</button>
     )
   }
 }
--- src/stores/ValueStore.js
+++ src/stores/ValueStore.js
@@ -1,2 +1,19 @@
 import AppDispatcher from '../AppDispatcher'
 import { EventEmitter } from 'events'
+
+class ValueStore extends EventEmitter {
+
+  startListening(cb) {
+    this.on('CHANGE', cb)
+  }
+
+  stopListening(cb) {
+    this.removeListener('CHANGE', cb)
+  }
+
+  getNumbers() {
+    return _operand;
+  }
+}
+
+export default new ValueStore();
